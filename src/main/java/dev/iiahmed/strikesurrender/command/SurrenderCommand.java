08fee251f2de77346b3151ac57ba33e3226ba9a2
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2022-07-01 15:42:31 +0200

    Add support for BestOf fights

--- src/main/java/dev/iiahmed/strikesurrender/command/SurrenderCommand.java
+++ src/main/java/dev/iiahmed/strikesurrender/command/SurrenderCommand.java
@@ -4,10 +4,13 @@ import dev.iiahmed.strikesurrender.util.Message;
 import ga.strikepractice.StrikePractice;
 import ga.strikepractice.api.StrikePracticeAPI;
 import ga.strikepractice.fights.Fight;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class SurrenderCommand implements CommandExecutor {
 
@@ -37,6 +40,23 @@ public class SurrenderCommand implements CommandExecutor {
             player.sendMessage(Message.USAGE.toString().replace("<command>", command.getName()));
             return true;
         }
+
+        /* If this is a bestof kit (Expremntial) */
+        List<String> teammates = fight.getTeammates(player);
+        if(fight.getKit().getBestOf() > 1 && teammates.size() == 1) {
+            Player firstOpponent = Bukkit.getPlayer(fight.getOpponents(player).get(0));
+            player.sendMessage(Message.SURRENDERED.toString());
+            api.forceWin(firstOpponent);
+            for(Player fightPlayer : fight.getPlayersInFight()) {
+                if(player == fightPlayer) {
+                    continue;
+                }
+                fightPlayer.sendMessage(Message.SURRENDER_ANNOUNCE.toString().replace("<player>", player.getName()));
+            }
+            return true;
+        }
+        /* ---- */
+
         player.setHealth(0);
         player.sendMessage(Message.SURRENDERED.toString());
         for(Player fightPlayer : fight.getPlayersInFight()) {
